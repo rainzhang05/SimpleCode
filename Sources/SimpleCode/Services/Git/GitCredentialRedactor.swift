ce856f425af4b2882a29a8ae1d4823ba8930f6b6
Rain Zhang <tza103@sfu.ca>
2026-07-15 03:43:49 -0700

    Remove unused GitCredentialRedactor.redactURL

--- Sources/SimpleCode/Services/Git/GitCredentialRedactor.swift
+++ Sources/SimpleCode/Services/Git/GitCredentialRedactor.swift
@@ -1,17 +1,6 @@
 import Foundation
 
 enum GitCredentialRedactor {
-    static func redactURL(_ input: String) -> String {
-        guard let url = URL(string: input), var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
-            return redactSCPLike(input)
-        }
-        if components.user != nil {
-            components.user = "REDACTED"
-            components.password = nil
-        }
-        return components.string ?? input
-    }
-
     static func redactText(_ input: String) -> String {
         var result = input
         // https://user:token@host
@@ -21,9 +10,5 @@ enum GitCredentialRedactor {
             result = regex.stringByReplacingMatches(in: result, range: range, withTemplate: "https://REDACTED@")
         }
         return result
-    }
-
-    private static func redactSCPLike(_ input: String) -> String {
-        input
     }
 }
