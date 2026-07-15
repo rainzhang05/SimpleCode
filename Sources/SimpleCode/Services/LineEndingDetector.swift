ebfc338582bd6d68b058be2072b85adc16c7c783
Rain Zhang <tza103@sfu.ca>
2026-07-15 01:20:41 -0700

    Optimize line ending detection memory usage

--- Sources/SimpleCode/Services/LineEndingDetector.swift
+++ Sources/SimpleCode/Services/LineEndingDetector.swift
@@ -6,23 +6,25 @@ enum LineEndingDetector {
         var crlf = 0
         var cr = 0
 
-        let utf16 = Array(text.utf16)
-        var index = 0
-        while index < utf16.count {
-            if utf16[index] == 13 {
-                if index + 1 < utf16.count, utf16[index + 1] == 10 {
+        var previousWasCR = false
+        for codeUnit in text.utf8 {
+            if codeUnit == 10 { // LF
+                if previousWasCR {
                     crlf += 1
-                    index += 2
-                    continue
+                    cr -= 1
+                } else {
+                    lf += 1
                 }
+                previousWasCR = false
+            } else if codeUnit == 13 { // CR
                 cr += 1
-            } else if utf16[index] == 10 {
-                lf += 1
+                previousWasCR = true
+            } else {
+                previousWasCR = false
             }
-            index += 1
         }
 
-        let kinds = [lf > 0, crlf > 0, cr > 0].filter { $0 }.count
+        let kinds = (lf > 0 ? 1 : 0) + (crlf > 0 ? 1 : 0) + (cr > 0 ? 1 : 0)
         if kinds > 1 { return .mixed }
         if crlf > 0 { return .crlf }
         if cr > 0 { return .cr }
