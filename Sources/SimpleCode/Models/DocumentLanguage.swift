dafaa52941f0c505223e9abeaca983181db0792c
Rain Zhang <tza103@sfu.ca>
2026-07-15 04:05:57 -0700

    Remove unused LanguageID.detect convenience wrapper

--- Sources/SimpleCode/Models/DocumentLanguage.swift
+++ Sources/SimpleCode/Models/DocumentLanguage.swift
@@ -1,9 +1,1 @@
-import Foundation
-
 typealias DocumentLanguage = LanguageID
-
-extension LanguageID {
-    static func detect(from url: URL, content: String = "") -> LanguageID {
-        LanguageDetector.detect(url: url, content: content)
-    }
-}
